5998c5724a3f3b13d8dbcffc65a6fb8f1e3a5b50
NeverSayNeverLove <NeverSayNeverLoveMD@gmail.com>
2018-07-13 11:09:33 +0700

    chapter_10

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,14 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update, :destroy]
+  before_action :correct_user, only: [:edit, :update]
+  before_action :admin_user, only: :destroy
+  before_action :find_by, only: [:show, :edit, :update, :correct_user, :destroy]
+
+  def index
+    @users = User.paginate page: params[:page],
+      per_page: Settings.paginate.per_page
+  end
+
   def new
     @user = User.new
   end
@@ -16,15 +26,54 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find_by id: params[:id]
     return if @user
     flash[:danger] = t "controller.user.show.notfind"
     redirect_to root_path
+  end
+
+  def edit; end
+
+  def update
+    if @user.update_attributes user_params
+      flash[:success] = t "controller.user.update.updated"
+      redirect_to @user
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    if @user.destroy
+      flash[:success] = t "controller.user.destroy.deleted"
+      redirect_to users_url
+    else
+      flash[:warning] = t "controller.user.update.update_false"
+      redirect_to root_path
+    end
   end
 
   private
   def user_params
     params.require(:user).permit :name, :email, :password,
       :password_confirmation
+  end
+
+  def logged_in_user
+    return if logged_in?
+    store_location
+    flash[:danger] = t "controller.user.logged.please_loggin"
+    redirect_to login_url
+  end
+
+  def correct_user
+    redirect_to root_url unless current_user? @user
+  end
+
+  def admin_user
+    redirect_to root_url unless current_user.admin?
+  end
+
+  def find_by
+    @users = User.paginate page: params[:page]
   end
 end
