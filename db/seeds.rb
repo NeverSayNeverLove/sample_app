d2b46d486b80a90b2445bd9f79a24e536dbdffee
NeverSayNeverLove <NeverSayNeverLoveMD@gmail.com>
2018-07-13 11:09:33 +0700

    chapter_11

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,11 @@
-User.create! name:  "Dang Nhu Thuy", email: "NeverSayNeverLove@railstutorial.org",
-  password: "123456", password_confirmation: "123456", admin: true
+User.create! name:  "Dang Nhu Thuy", email: "NeverSayNeverLoveMD@gmail.com",
+  password: "123456", password_confirmation: "123456", admin: true,
+  activated: true, activated_at: Time.zone.now
 
 99.times do |n|
 name  = Faker::Name.name
 email = "example-#{n+1}@railstutorial.org"
-password = "password"
+password = "123456"
 User.create! name: name, email: email, password: password,
-  password_confirmation: password
+  password_confirmation: password, activated: true, activated_at: Time.zone.now
 end
